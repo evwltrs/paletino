0e21263e4d19075eb507adf9f58a47e660b91d8b
Ev <ev@1nf.me>
2023-06-26 12:46:15 +0100

    move to lib.rs

--- src/main.rs
+++ src/main.rs
@@ -1,74 +1,12 @@
-extern crate image;
-
-use image::GenericImageView;
-
-#[derive(Clone, Debug)]
-struct Color {
-    r: u8,
-    g: u8,
-    b: u8,
-}
-
-impl Color {
-    fn new(r: u8, g: u8, b: u8) -> Color {
-        Color { r, g, b }
-    }
-}
-
-fn median_cut(image: &image::DynamicImage, num_colors: usize) -> Vec<Color> {
-    let pixels = image
-        .pixels()
-        .map(|p| {
-            let rgba = p.2;
-            Color::new(rgba[0], rgba[1], rgba[2])
-        })
-        .collect::<Vec<Color>>();
-
-    let mut colors = vec![pixels];
-
-    while colors.len() < num_colors {
-        let len = colors.len();
-        for i in 0..len {
-            let mut color_group = colors.swap_remove(i);
-            color_group.sort_by(|a, b| {
-                let ar = a.r as u32;
-                let ag = a.g as u32;
-                let ab = a.b as u32;
-                let br = b.r as u32;
-                let bg = b.g as u32;
-                let bb = b.b as u32;
-                (ar * ar + ag * ag + ab * ab).cmp(&(br * br + bg * bg + bb * bb))
-            });
-            let split_index = color_group.len() / 2;
-            let (group1, group2) = color_group.split_at_mut(split_index);
-            colors.push(group1.to_vec());
-            colors.push(group2.to_vec());
-        }
-    }
-
-    let mut palette = Vec::new();
-    for color_group in colors.iter() {
-        let sum_r: u32 = color_group.iter().map(|c| c.r as u32).sum();
-        let sum_g: u32 = color_group.iter().map(|c| c.g as u32).sum();
-        let sum_b: u32 = color_group.iter().map(|c| c.b as u32).sum();
-        let num_colors = color_group.len() as u32;
-        let avg_color = Color::new(
-            (sum_r / num_colors) as u8,
-            (sum_g / num_colors) as u8,
-            (sum_b / num_colors) as u8,
-        );
-        palette.push(avg_color);
-    }
-
-    palette
-}
+use paletino::median_cut;
 
 fn main() {
-    let image = image::open("sunset.jpg").unwrap();
+    let image = image::open("image.png").unwrap();
     let num_colors = 8; // Number of colors in the palette
 
     let palette = median_cut(&image, num_colors);
     for color in palette {
-        println!("{}, {}, {}", color.r, color.g, color.b);
+        // println!("{}, {}, {}", color.r, color.g, color.b);
+        println!("{:?}", color);
     }
 }
